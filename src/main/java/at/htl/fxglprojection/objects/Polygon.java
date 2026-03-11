3f09cc8a311c681335be8aebd751611c99226ae8
Illuminum2 <trost.zureden08@icloud.com>
2026-03-11 13:13:18 +0100

    Change name of polygon color to fillColor

--- src/main/java/at/htl/fxglprojection/objects/Polygon.java
+++ src/main/java/at/htl/fxglprojection/objects/Polygon.java
@@ -12,7 +12,7 @@ public class Polygon extends Component implements Vertices {
     private List<Point3D> points;
 
     // https://github.com/AlmasB/FXGLGames/blob/master/Breakout/src/main/java/com/almasb/fxglgames/breakout/components/BallComponent.java
-    private ObjectProperty<Color> color = new SimpleObjectProperty<>(Color.GREY);
+    private ObjectProperty<Color> fillColor = new SimpleObjectProperty<>(Color.GREY);
 
     public Polygon(List<Point3D> points) {
         this.points = points;
@@ -35,11 +35,11 @@ public class Polygon extends Component implements Vertices {
         return points;
     }
 
-    public Color getColor() {
-        return this.color.get();
+    public Color getFillColor() {
+        return this.fillColor.get();
     }
 
-    public void setColor(Color color) {
-        this.color.set(color);
+    public void setFillColor(Color fillColor) {
+        this.fillColor.set(fillColor);
     }
 }
