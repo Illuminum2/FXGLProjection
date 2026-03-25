901a65ddc8a488313275a7805d2343554475f8f4
Illuminum2 <trost.zureden08@icloud.com>
2026-03-25 10:13:13 +0100

    Add normal vector to polygon

--- src/main/java/at/htl/fxglprojection/objects/Polygon3D.java
+++ src/main/java/at/htl/fxglprojection/objects/Polygon3D.java
@@ -10,25 +10,27 @@ import at.htl.fxglprojection.projection.Vec3D;
 
 public class Polygon3D implements Vertices {
     private List<Vec3D> points;
+    private Vec3D normal;
 
     // https://github.com/AlmasB/FXGLGames/blob/master/Breakout/src/main/java/com/almasb/fxglgames/breakout/components/BallComponent.java
     private ObjectProperty<Color> fillColor = new SimpleObjectProperty<>(Color.GREY);
 
-    public Polygon3D(List<Vec3D> points) {
+    public Polygon3D(List<Vec3D> points, Vec3D normal) {
         this.points = points;
+        this.normal = normal;
     }
 
-    public Polygon3D(List<Vec3D> points, Color fillColor) {
-        this(points);
+    public Polygon3D(List<Vec3D> points, Vec3D normal, Color fillColor) {
+        this(points, normal);
         setFillColor(fillColor);
     }
 
-    public Polygon3D(List<Vec3D> points, int expectedVertexCount) {
-        this(validate(points, expectedVertexCount));
+    public Polygon3D(List<Vec3D> points, int expectedVertexCount, Vec3D normal) {
+        this(validate(points, expectedVertexCount), normal);
     }
 
-    public Polygon3D(List<Vec3D> points, int expectedVertexCount, Color fillColor) {
-        this(validate(points, expectedVertexCount), fillColor);
+    public Polygon3D(List<Vec3D> points, int expectedVertexCount, Vec3D normal, Color fillColor) {
+        this(validate(points, expectedVertexCount), normal, fillColor);
     }
 
     private static List<Vec3D> validate(List<Vec3D> points, int expected) {
