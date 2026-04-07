26073e2be102b643a42358f1e03ae7cbb790c340
Illuminum2 <trost.zureden08@icloud.com>
2026-04-07 09:26:36 +0200

    Implement changedSinceRead in Transform3DComponent

--- src/main/java/at/htl/fxglprojection/objects/Transform3DComponent.java
+++ src/main/java/at/htl/fxglprojection/objects/Transform3DComponent.java
@@ -1,5 +1,8 @@
 package at.htl.fxglprojection.objects;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.almasb.fxgl.entity.component.Component;
 
 import at.htl.fxglprojection.projection.Quaternion;
@@ -10,24 +13,39 @@ public final class Transform3DComponent extends Component {
     private Quaternion rotationQuat = new Quaternion();
     private Vec3D scale = new Vec3D(1, 1, 1);
 
+    private boolean[] changesSinceRead = new boolean[]{true, true, true};
+
     public Vec3D getPosition() {
+        changesSinceRead[0] = false;
         return position;
     }
     public void setPosition(Vec3D position) {
+        changesSinceRead[0] = true;
         this.position = position;
     }
 
     public Quaternion getRotationQuat() {
+        changesSinceRead[1] = false;
         return rotationQuat;
     }
     public void setRotationQuat(Quaternion rotationDeg) {
+        changesSinceRead[1] = true;
         this.rotationQuat = rotationDeg;
     }
 
     public Vec3D getScale() {
+        changesSinceRead[2] = false;
         return scale;
     }
     public void setScale(Vec3D scale) {
+        changesSinceRead[2] = true;
         this.scale = scale;
+    }
+
+    public boolean changedSinceRead() {
+        if (changesSinceRead[0] || changesSinceRead[1] || changesSinceRead[2])
+            return true;
+
+        return false;
     }
 }
