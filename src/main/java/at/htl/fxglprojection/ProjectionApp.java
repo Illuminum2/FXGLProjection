10e9eda11c20c52e94d64f27c2effb7447b3b332
Illuminum2 <trost.zureden08@icloud.com>
2026-05-15 23:15:45 +0200

    Switch to new RenderService and load demo obj

--- src/main/java/at/htl/fxglprojection/ProjectionApp.java
+++ src/main/java/at/htl/fxglprojection/ProjectionApp.java
@@ -1,8 +1,13 @@
 package at.htl.fxglprojection;
 
-import at.htl.fxglprojection.renderer.TempRenderService;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.SpawnData;
+
+import at.htl.fxglprojection.objects.ObjectFactory;
+import at.htl.fxglprojection.projection.Vec3D;
+import at.htl.fxglprojection.renderer.RenderService;
 
 public class ProjectionApp extends GameApplication {
     @Override
@@ -12,11 +17,17 @@ public class ProjectionApp extends GameApplication {
         settings.setTitle("Projection");
         settings.setVersion("0.1");
 
-        settings.addEngineService(TempRenderService.class);
+        settings.addEngineService(RenderService.class);
     }
 
     @Override
     protected void initGame() {
+        FXGL.getGameWorld().addEntityFactory(new ObjectFactory());
+
+        FXGL.spawn("objObject", new SpawnData()
+                .put("filepath", "src/main/resources/at/htl/fxglprojection/chicken.obj")
+                .put("position", new Vec3D(0, -95, 200))
+        );
     }
 
     static void main(String[] args) {
