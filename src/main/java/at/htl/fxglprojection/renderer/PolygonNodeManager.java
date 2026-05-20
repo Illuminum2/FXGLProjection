a95b44d726ed1ccefb0083a95cec35c282007563
Illuminum2 <trost.zureden08@icloud.com>
2026-05-20 23:11:39 +0200

    Move polygon synchronization logic to separate class

---
+++ src/main/java/at/htl/fxglprojection/renderer/PolygonNodeManager.java
@@ -0,0 +1,77 @@
+package at.htl.fxglprojection.renderer;
+
+import java.util.*;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+
+import at.htl.fxglprojection.objects.Polygon3D;
+
+public class PolygonNodeManager {
+    private final Pane renderLayer;
+    private final Map<Polygon3D, Polygon> polygonNodes = new HashMap<>();
+
+    PolygonNodeManager(Pane renderLayer) {
+        this.renderLayer = renderLayer;
+    }
+
+    void clear() {
+        polygonNodes.clear();
+    }
+
+    public void sync(List<ProjectedPolygon> polygons, ColorMode colorMode) {
+        Set<Polygon3D> visiblePolygons = new HashSet<>();
+        List<Polygon> sortedNodes = new ArrayList<>();
+
+        for (ProjectedPolygon projectedPolygon : polygons) {
+            Polygon fxPoly = polygonNodes.get(projectedPolygon.source());
+
+            if (fxPoly == null) {
+                fxPoly = new Polygon();
+                polygonNodes.put(projectedPolygon.source(), fxPoly);
+                renderLayer.getChildren().add(fxPoly);
+            }
+
+            updateFxPolygon(fxPoly, projectedPolygon, colorMode);
+            visiblePolygons.add(projectedPolygon.source());
+            sortedNodes.add(fxPoly);
+        }
+
+        // Remove old/out of view polygons
+        polygonNodes.entrySet().removeIf(p -> {
+            if (visiblePolygons.contains(p.getKey()))
+                return false;
+
+            renderLayer.getChildren().remove(p.getValue());
+            return true;
+        });
+
+        renderLayer.getChildren().setAll(sortedNodes);
+    }
+
+    private void updateFxPolygon(Polygon fxPoly, ProjectedPolygon pp, ColorMode colorMode) {
+        fxPoly.getPoints().setAll(pp.points());
+        fxPoly.setFill(calculateColor(pp, colorMode));
+        fxPoly.setStrokeWidth(2);
+    }
+
+    private Color calculateColor(ProjectedPolygon pp, ColorMode colorMode) {
+        if (colorMode == ColorMode.ORIGINAL)
+            return pp.source().getFillColor();
+        if (colorMode == ColorMode.NORMALS)
+            return Color.rgb(
+                    Math.abs((int) (pp.source().getNormal().x * 255)),
+                    Math.abs((int) (pp.source().getNormal().y * 255)),
+                    Math.abs((int) (pp.source().getNormal().z * 255))
+            );
+        if (colorMode == ColorMode.QUANTIZED_NORMALS)
+            return Color.rgb(
+                    Math.abs((int) (Math.round(pp.source().getNormal().x) * 255)),
+                    Math.abs((int) (Math.round(pp.source().getNormal().y) * 255)),
+                    Math.abs((int) (Math.round(pp.source().getNormal().z) * 255))
+            );
+
+        throw new IllegalArgumentException("Illegal color mode " + colorMode.name() + ".");
+    }
+}
--- src/main/java/at/htl/fxglprojection/renderer/RenderService.java
+++ src/main/java/at/htl/fxglprojection/renderer/RenderService.java
@@ -3,9 +3,7 @@ package at.htl.fxglprojection.renderer;
 import java.util.*;
 
 import javafx.scene.text.Text;
-import javafx.scene.paint.Color;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Polygon;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.core.EngineService;
 
@@ -19,7 +17,7 @@ public class RenderService extends EngineService {
 
 
     private final Pane renderLayer = new Pane();
-    private final Map<Polygon3D, Polygon> polygonNodes = new HashMap<>();
+    private final PolygonNodeManager polygonNodeManager = new PolygonNodeManager(renderLayer);
 
 
     private final Camera3DProjection camera = new Camera3DProjection();
@@ -38,9 +36,9 @@ public class RenderService extends EngineService {
 
 
     private boolean showFps = true;
-    private Text fpsText = new Text();
+    private final Text fpsText = new Text();
     private boolean showVerticesCount = true;
-    private Text verticesCountText = new Text();
+    private final Text verticesCountText = new Text();
 
     public boolean getShowFps() { return showFps; }
     public boolean getShowVerticesCount() { return showVerticesCount; }
@@ -79,7 +77,7 @@ public class RenderService extends EngineService {
 
         projectedPolygons.sort(Comparator.comparingDouble(ProjectedPolygon::depth).reversed());
 
-        syncNodes(projectedPolygons);
+        polygonNodeManager.sync(projectedPolygons, colorMode);
 
         // FPS counter
         if (showFps) {
@@ -104,63 +102,8 @@ public class RenderService extends EngineService {
     public void onExit() {
         renderLayer.getChildren().clear();
         FXGL.getGameScene().getContentRoot().getChildren().remove(renderLayer);
-        polygonNodes.clear();
+        polygonNodeManager.clear();
 
         initialized = false;
-    }
-
-    private void syncNodes(List<ProjectedPolygon> polygons) {
-        Set<Polygon3D> visiblePolygons = new HashSet<>();
-        List<Polygon> sortedNodes = new ArrayList<>();
-
-        for (ProjectedPolygon projectedPolygon : polygons) {
-            Polygon fxPoly = polygonNodes.get(projectedPolygon.source());
-
-            if (fxPoly == null) {
-                fxPoly = new Polygon();
-                polygonNodes.put(projectedPolygon.source(), fxPoly);
-                renderLayer.getChildren().add(fxPoly);
-            }
-
-            updateFxPolygon(fxPoly, projectedPolygon);
-            visiblePolygons.add(projectedPolygon.source());
-            sortedNodes.add(fxPoly);
-        }
-
-        // Remove old/out of view polygons
-        polygonNodes.entrySet().removeIf(p -> {
-            if (visiblePolygons.contains(p.getKey()))
-                return false;
-
-            renderLayer.getChildren().remove(p.getValue());
-            return true;
-        });
-
-        renderLayer.getChildren().setAll(sortedNodes);
-    }
-
-    private void updateFxPolygon(Polygon fxPoly, ProjectedPolygon pp) {
-        fxPoly.getPoints().setAll(pp.points());
-        fxPoly.setFill(calculateColor(pp));
-        fxPoly.setStrokeWidth(2);
-    }
-
-    private Color calculateColor(ProjectedPolygon pp) {
-        if (colorMode == ColorMode.ORIGINAL)
-            return pp.source().getFillColor();
-        if (colorMode == ColorMode.NORMALS)
-            return Color.rgb(
-                    Math.abs((int) (pp.source().getNormal().x * 255)),
-                    Math.abs((int) (pp.source().getNormal().y * 255)),
-                    Math.abs((int) (pp.source().getNormal().z * 255))
-            );
-        if (colorMode == ColorMode.QUANTIZED_NORMALS)
-            return Color.rgb(
-                    Math.abs((int) (Math.round(pp.source().getNormal().x) * 255)),
-                    Math.abs((int) (Math.round(pp.source().getNormal().y) * 255)),
-                    Math.abs((int) (Math.round(pp.source().getNormal().z) * 255))
-            );
-
-        throw new IllegalArgumentException("Illegal color mode " + colorMode.name() + ".");
     }
 }
